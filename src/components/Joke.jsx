5a77f4c073d5589ef06bf158bfd02d8eb7cc0d80
mkubera <rainteller@tutanota.de>
2021-11-01 20:26:56 +0100

    Deleted fetch

--- src/components/Joke.jsx
+++ src/components/Joke.jsx
@@ -3,12 +3,7 @@ import { useState, useEffect } from "react";
 const Joke = () => {
   const [joke, setJoke] = useState(""); // => [state, changeStateFn]
 
-  useEffect(() => {
-    fetch("https://api.chucknorris.io/jokes/random")
-      .then((resp) => resp.json())
-      .then((resp) => setJoke(resp.value))
-      .catch((err) => console.error(err));
-  }, []);
+  useEffect(() => {}, []);
 
   return <p>{joke}</p>;
 };
