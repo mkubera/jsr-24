cf2c4244666fcf6a8a92600fcdb84f894b8328a2
mkubera <rainteller@tutanota.de>
2021-11-08 19:14:15 +0100

    Deleted all fruits data

---
+++ src/components/Fruits.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+const Fruits = () => {
+  const INIT_SHOW_COUNT = 0;
+  const [showCount, setShowCount] = useState(INIT_SHOW_COUNT);
+
+  const fruits = [];
+
+  return (
+    <>
+      <ul>
+        {fruits
+          .filter((f, i) => i <= showCount)
+          .map(({ id, name, isNew }) => (
+            <li key={id}>
+              {name} {isNew ? "nowosc" : ""}
+            </li>
+            // <Fruit {...f} />
+          ))}
+      </ul>
+      <button onClick={(e) => setShowCount(showCount + 1)}>Show More</button>
+    </>
+  );
+};
+
+export default Fruits;
